d68b4e4a61af9665e9b0d1647bc52d035facc575
Tapajyoti Ghosh <tapajyoti.ghosh@intglobal.com>
2025-08-20 18:34:23 +0530

    jwt-work

---
+++ src/main/java/com/springsecurity/controllers/AuthController.java
@@ -0,0 +1,32 @@
+package com.springsecurity.controllers;
+
+import com.springsecurity.dto.LoginRequestDto;
+import com.springsecurity.dto.LoginResponseDto;
+import com.springsecurity.dto.SignupResponseDto;
+import com.springsecurity.security.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+
+     return ResponseEntity.ok(authService.login(loginRequestDto));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignupResponseDto> signup(@RequestBody LoginRequestDto signupRequestDto) {
+
+        return ResponseEntity.ok(authService.signup(signupRequestDto));
+    }
+}
--- src/main/java/com/springsecurity/entity/User.java
+++ src/main/java/com/springsecurity/entity/User.java
@@ -1,10 +1,7 @@
 package com.springsecurity.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,6 +13,8 @@ import java.util.List;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@Table(name = "app_user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
